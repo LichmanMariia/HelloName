23bf76c0a8b0a544571c335b61eb7274a508be89
student <student@K04-15.lan.itstep.kiev.ua>
2018-05-10 19:47:56 +0300

    my first commit

---
+++ src/org/itstep/AppRunner.java
@@ -0,0 +1,13 @@
+package org.itstep;
+
+public class AppRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Name firstName = new Name();
+String myName = firstName.getName("Tata"); 
+System.out.println(myName);
+
+	}
+
+}
---
+++ src/org/itstep/Name.java
@@ -0,0 +1,8 @@
+package org.itstep;
+
+public class Name {
+public String getName(String name) {
+	
+	return "Hello "+ name;
+}
+}
